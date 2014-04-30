64df80b998194bfc40c1327ff7c476756ed3a61e
Orion Engleton - MBAir <orion.engleton@gmail.com>
2014-04-30 02:40:01 -0400

    Added tests for location and refactored - the logic in the Location class

--- lib/location.rb
+++ lib/location.rb
@@ -1,2 +1,39 @@
 class Location
+  R = 3_959
+
+  attr_reader :longitude, :latitude
+  
+  def initialize(longitude: longitude, latitude: latitude)
+    @longitude = longitude
+    @latitude  = latitude
+  end
+
+  def near?(latitude: latitude, longitude: longitude, radius: radius)
+    raise ArgumentError unless radius >= 0
+    location = Location.new(latitude: latitude, longitude: longitude)      
+    (R * haversine_distance(location)) <= radius
+
+  end
+
+  private
+
+  def to_radians(degrees)
+    degrees * Math::PI / 180
+  end
+
+  def haversine_distance(location)
+    dist_lat  = to_radians(location.latitude - self.latitude)
+    dist_long = to_radians(location.longitude - self.longitude)
+
+    lat1      = to_radians(self.latitude)
+    lat2      = to_radians(location.latitude)
+
+    # Formula for checking ??
+    _a =   Math.sin(dist_lat/2) * Math.sin(dist_lat/2) + 
+           Math.sin(dist_long) * Math.sin(dist_long/2) *
+           Math.cos(lat1) * Math.cos(lat2)
+    
+    _b =  2 * Math.atan2(Math.sqrt(_a), Math.sqrt(1-_a))
+  end
+
 end
--- spec/lib/location_spec.rb
+++ spec/lib/location_spec.rb
@@ -1,4 +1,57 @@
 require_relative '../spec_helper'
 
 describe Location do 
+    subject { Location.new(latitude: 38.911268, longitude: -77.444243) }    
+    its (:latitude)   { should == 38.911268  }
+    its (:longitude)  { should == -77.444243 }
+   
+    let(:airspace) { Location.new(latitude: 38.911268, longitude: -77.444243) }
+
+    let(:latitude) { 38.911268 }
+    let(:longitude) { -77.444243 }
+
+  describe '#initialize' do 
+    it 'sets the latitude to 0 and longitude to 1' do 
+      loc = Location.new(latitude: 0, longitude: 1)
+      loc.latitude.should == 0
+      loc.longitude.should == 1
+    end
+
+    subject { airspace }
+
+    its (:latitude) { should == latitude  }
+    its (:longitude)  { should == longitude }
+  end
+
+  describe '#near?' do 
+    context "when within the specified radius" do 
+      subject { airspace.near?(latitude: latitude, longitude: longitude, radius: 10)}
+      it { should be_true }
+    end
+
+     context "when outside the specified radius" do 
+        subject { airspace.near?(latitude: latitude * 10, longitude: longitude * 10, radius: 1)}
+        it {should be_false}
+     end
+
+     context "when within airspace" do 
+      subject { airspace }
+      it { should be_near(latitude: latitude, longitude: longitude, radius: 1)}
+     end
+
+     context "when outside airspace" do
+      subject { airspace }
+      it { should_not be_near(latitude: latitude * 10, longitude: longitude * 10, radius: 1)}
+     end
+
+     context "when a negative radius is used" do
+      it 'Raises an error' do 
+          expect { airspace.near?(latitude: latitude, longitude: longitude, radius: -1 )}
+          .to raise_error ArgumentError
+      end
+     end
+
+  end
+
+
 end
