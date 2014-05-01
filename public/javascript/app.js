2374e300146f1c42d257aab75ff9d162e7db1136
Orion Engleton - MBAir <orion.engleton@gmail.com>
2014-05-01 14:11:52 -0400

    Removed crufty methods I wrote - checkCity early on

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,15 +1,30 @@
 // The Angular Appplication.
 var locationFinder = angular.module('placesNearMe', [])
 
-locationFinder.controller('PlacesCtrl', ['$scope', '$window', function($scope, $window){
+locationFinder.controller('PlacesCtrl', ['$scope', '$window', '$http', function($scope, $window, $http){
   $scope.position = null;
+  $scope.address = null;
 
   $scope.getLocation = function() {
     
     window.navigator.geolocation.getCurrentPosition( 
+      
       function(position) {
           $scope.$apply(function() { 
-            $scope.position = position; 
+            $scope.position = position;
+            
+            var _coords = { latitude: position.coords.latitude, 
+                            longitude: position.coords.longitude }
+            console.log(_coords)
+
+            $http({ url: '/locations', 
+                    data: JSON.stringify(_coords), 
+                    method: 'POST',
+                    headers: 'Content-Type: application/json'
+              
+            }).success(function(data) {
+              $scope.locations  = data;
+            })
           }); 
         }, 
         function(error) {
@@ -17,9 +32,26 @@ locationFinder.controller('PlacesCtrl', ['$scope', '$window', function($scope, $
       });
   };
 
+
+  $scope.checkCity = function() {
+    if ($scope.another.city) {
+      console.log( $scope.another.city);
+      $http({ url: '/locations', 
+              data: JSON.stringify( {check_city: $scope.another.city}), 
+              method: 'POST', 
+              headers: 'Content-Type: application/json'
+            }).success(function(data) {
+              $scope.locations = data;
+              console.log(data)
+          })
+    }
+  }
 }])
 
 
+locationFinder.controller('MorePlacesCtrl', ['$scope', function($scope) {
 
-
-
+  $scope.checkCity = function() {
+    console.log('This is Nice');
+  }
+}])
