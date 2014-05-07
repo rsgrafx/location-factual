ae76da4b8a50e8e084cc0fef7cd5d24fd856350c
Orion Engleton - MBAir <orion.engleton@gmail.com>
2014-05-07 13:39:06 -0400

    Added a new controller to this app - NavCtrl - this controller manages the logic in the navbar In the controller  - added two functions - logOutUser and logged_in() Added a run scope config that checks whether on each route path change that the session state is still the same - else the user needs to log in again

--- public/javascript/app.dashboard/index.js
+++ public/javascript/app.dashboard/index.js
@@ -11,11 +11,34 @@ var LocationFinderDashboard = angular.module('LocationFinderDashboard', [
   'locationFinder.directives',
   'SessionService',
   'dashboard.controllers'
+  ,'dashboard.AuthTokenInterceptor'
   ])
+.controller('NavCtrl', ['$scope', 'Session', function($scope, Session){
+  $scope.logged_in = function() {
+    return !!Session.isAuthenticated();
+  }
+  $scope.logOutUser = function() {
+    return Session.logout();
+  }
+  
+}])
 .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $locationProvider.hashPrefix('!');
     $routeProvider
+      .when('/profile', {templateUrl: 'views/dashboard/profile.html'})
+      .when('/welcome', { templateUrl: 'views/dashboard/welcome.html'})
       .when('/login',   { templateUrl: 'views/dashboard/login.html', controller: 'UsersCtrl' })
-      .when('/register',{templateUrl: 'views/dashboard/register.html'})
-      .otherwise({templateUrl:'views/dashboard/main.html'})
-  }])
+      .when('/register',{templateUrl:  'views/dashboard/register.html'})
+      .otherwise({templateUrl:'views/dashboard/welcome.html'})
+}])
+.run(['$rootScope', '$location', 'Session', function($rootScope, $location, Session) {
+
+  $rootScope.$on('$routeChangeStart', function(event) {
+    // if not logged in.
+    if (!Session.isAuthenticated()) {
+      console.log('watch out thief');
+      event.preventDefault();
+      $location.path('/login')
+    }
+  })
+}])
