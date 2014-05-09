f43fbe89e3328fc51dfbb5a7b5916ea4f1a35114
Orion Engleton - MBAir <orion.engleton@gmail.com>
2014-05-09 13:11:05 -0400

    Removed the old routeProvider logic within the config function and also * removed the run function that was causing a  infinite loop to be called. Move authentication logic to the UsersCtrl

--- public/javascript/app.dashboard/index.js
+++ public/javascript/app.dashboard/index.js
@@ -6,46 +6,18 @@
 * Description
 */
 
-var LocationFinderDashboard = angular.module('LocationFinderDashboard', [ 
-  'ngRoute',
-  'locationFinder.directives',
+var lfinder = angular.module('LocationFinderDashboard', [ 
   'SessionService',
   'dashboard.controllers',
-  'dashboard.AuthTokenInterceptor'
+  'dashboard.AuthTokenInterceptor',
+  'dashboard.routing'
   ])
 .controller('NavCtrl', ['$scope', 'Session', function($scope, Session){
-  $scope.logged_in = function() {
+    $scope.logged_in = function() {
     return !!Session.isAuthenticated();
   }
+  
   $scope.logOutUser = function() {
     return Session.logout();
-  }
-  
-}])
-.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $locationProvider.hashPrefix('!');
-    $routeProvider
-      .when('/guides/new',{ templateUrl: 'views/dashboard/new.guide.html'})
-      .when('/profile',   { templateUrl: 'views/dashboard/profile.html'})
-      .when('/guides',    { templateUrl: 'views/dashboard/guides.html'})
-      .when('/welcome',   { templateUrl: 'views/dashboard/welcome.html'})
-      .when('/login',     { templateUrl: 'views/dashboard/login.html',    controller: 'UsersCtrl', access: true })
-      .when('/register',  { templateUrl:  'views/dashboard/register.html', controller: 'UsersCtrl', access: true})
-      .otherwise({templateUrl:'views/dashboard/welcome.html'})
-}])
-.run(['$rootScope', '$location', 'Session', function($rootScope, $location, Session) {
-
-  $rootScope.$on('$routeChangeStart', function(event, next, current) {
-    // if not logged in.
-    if (!Session.isAuthenticated()) {
-      console.log('watch out thief');
-      event.preventDefault();
-     if (next.access) {
-        console.log(next.$$route.originalPath)
-        $location.path(next.$$route.originalPath)
-      } else {
-        $location.path('/login')
-      }
-    }
-  })
+  }  
 }])
