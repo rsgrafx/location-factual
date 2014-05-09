0c344e5c65a598c5805a736226cc9c141c2b09d1
Orion Engleton - MBAir <orion.engleton@gmail.com>
2014-05-08 21:57:50 -0400

    Added several routes to the dashboard - /guides/new /profile and /guides

--- public/javascript/app.dashboard/index.js
+++ public/javascript/app.dashboard/index.js
@@ -10,8 +10,8 @@ var LocationFinderDashboard = angular.module('LocationFinderDashboard', [
   'ngRoute',
   'locationFinder.directives',
   'SessionService',
-  'dashboard.controllers'
-  ,'dashboard.AuthTokenInterceptor'
+  'dashboard.controllers',
+  'dashboard.AuthTokenInterceptor'
   ])
 .controller('NavCtrl', ['$scope', 'Session', function($scope, Session){
   $scope.logged_in = function() {
@@ -25,10 +25,12 @@ var LocationFinderDashboard = angular.module('LocationFinderDashboard', [
 .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $locationProvider.hashPrefix('!');
     $routeProvider
-      .when('/profile', { templateUrl: 'views/dashboard/profile.html'})
-      .when('/welcome', { templateUrl: 'views/dashboard/welcome.html'})
-      .when('/login',   { templateUrl: 'views/dashboard/login.html',  controller: 'UsersCtrl', access: true })
-      .when('/register',{templateUrl:  'views/dashboard/register.html', controller: 'UsersCtrl', access: true})
+      .when('/guides/new',{ templateUrl: 'views/dashboard/new.guide.html'})
+      .when('/profile',   { templateUrl: 'views/dashboard/profile.html'})
+      .when('/guides',    { templateUrl: 'views/dashboard/guides.html'})
+      .when('/welcome',   { templateUrl: 'views/dashboard/welcome.html'})
+      .when('/login',     { templateUrl: 'views/dashboard/login.html',    controller: 'UsersCtrl', access: true })
+      .when('/register',  { templateUrl:  'views/dashboard/register.html', controller: 'UsersCtrl', access: true})
       .otherwise({templateUrl:'views/dashboard/welcome.html'})
 }])
 .run(['$rootScope', '$location', 'Session', function($rootScope, $location, Session) {
